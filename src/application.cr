e7b0bb1650f0878afc06afc435932174db256074
freyamade <frybrdrck@gmail.com>
2019-03-09 15:55:32 +0000

    Scrolling the logbox now works with Ctrl-W and Ctrl-S

--- src/application.cr
+++ src/application.cr
@@ -18,13 +18,11 @@ module Crcophony
     def initialize(@client : Discord::Client, channel : Discord::Snowflake)
       @screen = Hydra::TerminalScreen.new
       @app = Hydra::Application.setup screen: @screen
+      # Read the channel data from the cache
+      @channel = @client.cache.not_nil!.resolve_channel(channel).not_nil!
 
-      # Set up a ready listener that auto adds focus to the prompt
-      # Give focus to the message entry prompt on start up
-      @app.bind("ready") do |event_hub, _, _, _|
-        event_hub.focus "prompt"
-        false
-      end
+      # Set up necessary bindings
+      setup_bindings
 
       # Set up the elements in the application display
       # Channel name text display
@@ -34,31 +32,28 @@ module Crcophony
         :height   => "1",
         :value    => "", # This will be filled in later
       })
-      @app.add_element @channel_name
 
       # Messages container
       @messages = Hydra::Logbox.new("messages", {
         :position => "2:0",
         :width    => @screen.width.to_s,
-        :height   => (@screen.height - 3).to_s,
+        :height   => (@screen.height - 4).to_s,
       })
-      @app.add_element @messages
 
       # Message Prompt
       @prompt = Hydra::Prompt.new("prompt", {
         :position => "#{@screen.height - 3}:0",
         :width    => @screen.width.to_s,
-        :height   => "1",
+        :height   => "2",
       })
-      @app.add_element @prompt
 
-      # Read the channel data from the cache
-      @channel = @client.cache.not_nil!.resolve_channel(channel).not_nil!
+      # Add the elements to the application, ensuring to add messages last so the scrollbar is visible
+      @app.add_element @channel_name
+      @app.add_element @prompt
+      @app.add_element @messages
+
       # Set the initial server label
       set_channel @channel
-      # Set up various keybinds
-
-      setup_keybinds
     end
 
     # Wrapper around @app.run
@@ -73,15 +68,20 @@ module Crcophony
 
     # Change the app to display the new channel in the specified server
     #
-    # This function updates the top label, clears the old message box and retrieves some history for context (TODO)
+    # This function updates the top label, clears the old message box and retrieves some (50) messages for context
     def set_channel(channel : Discord::Channel)
       @channel = channel
       server = @client.cache.not_nil!.resolve_guild(@channel.guild_id.not_nil!)
       @channel_name.value = generate_label server, channel
       @messages.clear
+
       # Retrieve a message history
       @client.get_channel_messages(@channel.id).reverse.each do |message|
         handle_message message
+      end
+      # Scroll to the bottom
+      while @messages.can_scroll_down?
+        @messages.scroll -1
       end
     end
 
@@ -95,20 +95,38 @@ module Crcophony
       end
     end
 
-    # Set up the various keybinds used within the system
+    # Set up the various bindings used within the system
     #
+    # ## Keybinds
     # - <kbd>Ctrl</kbd>+<kbd>C</kbd>: Quit Application
     # - <kbd>Enter</kbd>: Send Message
-    private def setup_keybinds
+    # - <kbd>Ctrl</kbd>+<kbd>W</kbd>: Scroll Up
+    # - <kbd>Ctrl</kbd>+<kbd>S</kbd>: Scroll Down
+    private def setup_bindings
+      # Set up a ready listener that auto adds focus to the prompt
+      # Give focus to the message entry prompt on start up
+      @app.bind("ready") do |event_hub, _, _, _|
+        event_hub.focus "prompt"
+        false
+      end
       # Close the application on Ctrl+C
       @app.bind("keypress.ctrl-c", "application", "stop")
       # Send message on Enter
-      @app.bind("prompt", "keypress.enter") do |event_hub|
+      @app.bind("prompt", "keypress.enter") do |event_hub, _, elements, _|
         # send to discord
-        message = @prompt.value.strip
+        message = elements.by_id("prompt").as(Hydra::Prompt).value.strip
         next false unless message.size > 0
         @client.create_message @channel.id, message
         event_hub.trigger "prompt", "clear"
+        false
+      end
+      # Scroll up to scroll box
+      @app.bind("prompt", "keypress.") do |event_hub|
+        event_hub.trigger "messages", "scroll_up"
+        false
+      end
+      @app.bind("prompt", "keypress.") do |event_hub|
+        event_hub.trigger "messages", "scroll_down"
         false
       end
     end
