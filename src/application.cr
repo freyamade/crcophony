14bd095d33be036ed325f0361f18621a1ff4b129
freyamade <frybrdrck@gmail.com>
2019-03-09 02:38:12 +0000

    Decoupled application code from main code

---
+++ src/application.cr
@@ -0,0 +1,121 @@
+require "colorize"
+require "discordcr"
+require "hydra"
+require "./extensions/*"
+
+module Crcophony
+  # Class that manages the display and all stuff related to it
+  #
+  # This class also provides helper methods for cleanly updating parts of the display
+  class Application
+    @app : Hydra::Application
+    @channel : Discord::Channel
+    @channel_name : Hydra::Text
+    @client : Discord::Client
+    @messages : Hydra::Logbox
+    @prompt : Hydra::Prompt
+    @screen : Hydra::TerminalScreen
+
+    def initialize(@client : Discord::Client, channel : Discord::Snowflake)
+      @screen = Hydra::TerminalScreen.new
+      @app = Hydra::Application.setup screen: @screen
+
+      # Set up a ready listener that auto adds focus to the prompt
+      # Give focus to the message entry prompt on start up
+      @app.bind("ready") do |event_hub, _, _, _|
+        event_hub.focus "prompt"
+        false
+      end
+
+      # Set up the elements in the application display
+      # Channel name text display
+      @channel_name = Hydra::Text.new("channel_name", {
+        :position => "0:0",
+        :width    => @screen.width.to_s,
+        :height   => "1",
+        :value    => "", # This will be filled in later
+      })
+      @app.add_element @channel_name
+
+      # Messages container
+      @messages = Hydra::Logbox.new("messages", {
+        :position => "2:0",
+        :width    => @screen.width.to_s,
+        :height   => (@screen.height - 3).to_s,
+      })
+      @app.add_element @messages
+
+      # Message Prompt
+      @prompt = Hydra::Prompt.new("prompt", {
+        :position => "#{@screen.height - 3}:0",
+        :width    => @screen.width.to_s,
+        :height   => "1",
+      })
+      @app.add_element @prompt
+
+      # Read the channel data from the cache
+      @channel = @client.cache.not_nil!.resolve_channel(channel).not_nil!
+      # Set the initial server label
+      set_channel @channel
+      # Set up various keybinds
+
+      setup_keybinds
+    end
+
+    # Wrapper around @app.run
+    def run
+      @app.run
+    end
+
+    # Wrapper around @app.teardown
+    def teardown
+      @app.teardown
+    end
+
+    # Change the app to display the new channel in the specified server
+    #
+    # This function updates the top label, clears the old message box and retrieves some history for context (TODO)
+    def set_channel(channel : Discord::Channel)
+      @channel = channel
+      server = @client.cache.not_nil!.resolve_guild(@channel.guild_id.not_nil!)
+      @channel_name.value = generate_label server, channel
+      @messages.clear
+      # Load recent messages (TODO)
+    end
+
+    # Handler for receiving a message via the Discord client
+    def handle_message(message : Discord::Message)
+      if message.channel_id == @channel.id
+        # First do the various parsing and escaping we need to do
+        # Then add the message to the logbox
+        content = Crcophony::MessageParser.parse message, @client.client_id.to_u64
+        @messages.add_message "#{message.timestamp.to_s "%H:%M:%S"} #{message.author.username}: #{content}"
+      end
+    end
+
+    # Set up the various keybinds used within the system
+    #
+    # - <kbd>Ctrl</kbd>+<kbd>C</kbd>: Quit Application
+    # - <kbd>Enter</kbd>: Send Message
+    private def setup_keybinds
+      # Close the application on Ctrl+C
+      @app.bind("keypress.ctrl-c", "application", "stop")
+      # Send message on Enter
+      @app.bind("prompt", "keypress.enter") do |event_hub|
+        # send to discord
+        message = @prompt.value.strip
+        next false unless message.size > 0
+        @client.create_message @channel.id, message
+        event_hub.trigger "prompt", "clear"
+        false
+      end
+    end
+
+    # Generate a centered channel name given a server and a channel
+    private def generate_label(server : Discord::Guild, channel : Discord::Channel) : String
+      label = "#{server.name}##{channel.name}"
+      padding = (@screen.width - label.size) / 2
+      return "#{" " * padding}#{label}"
+    end
+  end
+end
--- src/crcophony.cr
+++ src/crcophony.cr
@@ -22,81 +22,12 @@ module Crcophony
   cache = Discord::Cache.new(client)
   client.cache = cache
 
-  # Create our own screen and pass it to the setup
-  screen = Hydra::TerminalScreen.new
-  view = Hydra::View.new height: screen.height, width: screen.width
-  view.filters << Hydra::BorderFilter
-  # Initialize an instance of an app
-  app = Hydra::Application.setup screen: screen, view: view
-
-  # Once the application is running, pressing ctrl-c will stop it.
-  app.bind("keypress.ctrl-c", "application", "stop")
-
-  # Set up basic elements
-
-  # Server#channel label
-  server_label_text = "Test#test"
-  padding = (screen.width - server_label_text.size) / 2
-  server_label = Hydra::Text.new("channel_name", {
-    :position => "0:0",
-    :width    => screen.width.to_s,
-    :height   => "1",
-    :value    => "#{" " * padding}#{server_label_text}",
-  })
-  app.add_element server_label
-
-  # LogBox to hold messages
-  message_container = Hydra::Logbox.new("history", {
-    :position => "2:0",
-    :width    => screen.width.to_s,
-    :height   => (screen.height - 3).to_s,
-  })
-  app.add_element message_container
-
-  # Prompt to type messages to send them
-  message_prompt = Hydra::Prompt.new("message-prompt", {
-    :position => "#{screen.height - 3}:0",
-    :width    => screen.width.to_s,
-    :height   => "1",
-  })
-  app.add_element message_prompt
+  # Create a Crcophony Application instance
+  app = Crcophony::Application.new client, channel
 
   # Add message handling to the Discord bot
   client.on_message_create do |payload|
-    logger.info "Message received: #{payload.to_s}"
-    if payload.channel_id == channel_id
-      content = payload.content
-      # handle escaping certain symbols
-      content = content.gsub "<", "\\<"
-      # parse the message for mentions
-      if payload.mentions.size > 0
-        payload.mentions.each do |user|
-          replace_string : String
-          if user.id == user_id
-            replace_string = "@#{user.username}".colorize.on_yellow.to_s
-          else
-            replace_string = "@#{user.username}"
-          end
-          content = content.gsub "\\<@!#{user.id}>", replace_string
-        end
-      end
-      message_container.add_message "#{payload.timestamp.to_s "%H:%M:%S"} #{payload.author.username}: #{content}"
-    end
-  end
-
-  # Give focus to the message entry prompt on start up
-  app.bind("ready") do |event_hub, _, elements, state|
-    event_hub.focus "message-prompt"
-    false
-  end
-
-  # When a message is sent, add it to the logbox along with the username
-  app.bind("message-prompt", "keypress.enter") do |event_hub|
-    # send to discord
-    next false unless message_prompt.value
-    client.create_message channel, message_prompt.value
-    event_hub.trigger "message-prompt", "clear"
-    false
+    app.handle_message payload
   end
 
   # start the client in a separate thread
