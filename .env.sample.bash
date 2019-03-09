543d7080e11da52bca43789b37566895500ef0ab
freyamade <frybrdrck@gmail.com>
2019-03-09 16:22:27 +0000

    Renamed the fish sample file and provided a bash sample file too

---
+++ .env.sample.bash
@@ -0,0 +1,8 @@
+# Sample environment file for use in the bash shell
+
+# channel_id
+export CRCOPHONY_CHANNEL_ID=""
+# token
+export CRCOPHONY_TOKEN=""
+# user id
+export CRCOPHONY_USER_ID=""
--- .env.sample.fish
+++ .env.sample.fish
@@ -1,4 +1,8 @@
 # Sample environment file for use in the fish shell
+
+# channel_id
 set -x CRCOPHONY_CHANNEL_ID ""
+# token
 set -x CRCOPHONY_TOKEN ""
+# user id
 set -x CRCOPHONY_USER_ID ""
