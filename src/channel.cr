f976077555a1b4df95ea075b16b418c8ee75ddc8
freyamade <frybrdrck@gmail.com>
2019-03-11 17:56:23 +0000

    Adding a Crcophony::Channel class to make things a little easier

---
+++ src/channel.cr
@@ -0,0 +1,22 @@
+require "discordcr"
+
+module Crcophony
+  # A wrapper class around Discord's channel class that makes things a little easier in this project
+  class Channel
+    @channel : Discord::Channel
+    @server : Discord::Guild
+    @unread_messages : UInt64 = 0_u64
+
+    def initialize(@channel : Discord::Channel, @server : Discord::Server)
+    end
+
+    def to_s : String
+      builder = String::Builder.new
+      builder << "#{@server.name}##{@channel.name}"
+      if @unread_messages > 0
+        builder << " [#{@unread_messages}]"
+      end
+      return builder.to_s
+    end
+  end
+end
