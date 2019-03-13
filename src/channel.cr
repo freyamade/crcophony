fcc9da4e1586a100ae441603f7c2b48dec04d13f
freyamade <frybrdrck@gmail.com>
2019-03-13 16:16:31 +0000

    Expanding channel to be able to handle channels without guilds (DMs)

--- src/channel.cr
+++ src/channel.cr
@@ -4,15 +4,23 @@ module Crcophony
   # A wrapper class around Discord's channel class that makes things a little easier in this project
   class Channel
     @channel : Discord::Channel
-    @server : Discord::Guild
+    @server : Discord::Guild?
     @unread_messages : UInt64 = 0_u64
+
+    # Private Channels do not have guild ids
+    def initialize(@channel : Discord::Channel)
+    end
 
     def initialize(@channel : Discord::Channel, @server : Discord::Guild)
     end
 
     def to_s : String
       builder = String::Builder.new
-      builder << "#{@server.name}##{@channel.name}"
+      if !@server.nil?
+        builder << "#{@server.not_nil!.name}##{@channel.name}"
+      else
+        builder << @channel.name
+      end
       if @unread_messages > 0
         builder << " [#{@unread_messages}]"
       end
