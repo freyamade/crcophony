ca033ca0b3c977bc351ed4cc2110fb45ac942d87
freya <fb@cix.ie>
2019-10-31 12:51:57 +0000

    Remove the build.sh script

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# A basic build script that *should* build the system in static release mode
-# Break on first error
-set -e
-# First, build the local image
-docker build -t crcophony_builder .
-docker run --rm -it -v $PWD:/app -w /app crcophony_builder shards build --static --release
